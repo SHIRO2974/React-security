9d45553521f3f8c22f73c85cf33b6ab415d5a119
SHIRO <wogus2974@naver.com>
2025-02-19 10:00:00 +0900

    SignIn SignUp 수업자료

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,30 +1,41 @@
 import { Box, Button, Card, CardContent, Container, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
-import { api } from '../../api/config/axiosConfig';
+import { Link, useNavigate } from 'react-router-dom';
+import { api, setAccessToken } from '../../api/config/axiosConfig';
+import { useQueryClient } from '@tanstack/react-query';
 
-function Signin(props) {
+/**
+ * 로그인 요구사항
+ * 각 필드가 공백인지만 체크(공백이면 아래 오류 메세지로 출력)
+ * 로그인 버튼 클릭시 /api/auth/signin 요청 
+ * -> 응답받은 AccessToken을 localstorage에 AccessToken이라는 키값으로 저장. 
+ * Index페이지로 이동.
+ * 계정이 없으신가요? 회원가입
+ * 
+ */
 
-    
-    const [ signinInput, setSigninInput ] = useState({  // 로그인인 초기값 설정
+function SigninPage(props) {
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+
+    const [ signinInput, setSigninInput ] = useState({
         username: "",
         password: "",
     });
 
-    const [ errors, setErrors ] = useState({    // 각 입력 필드의 error 초기값 설정
-            username: "",
-            password: "",
-        });
-    
+    const [ errors, setErrors ] = useState({
+        username: "",
+        password: "",
+    });
+
+    const [ isSigninError, setSigninError ] = useState(false);
 
     const handleInputOnBlur = (e) => {
-
         const { name, value } = e.target;
-        setErrors(prev =>({
-            ...prev
-        
-        }))
-       
-        
+        setErrors(prev => ({
+            ...prev,
+            [name]: !(value.trim()) ? `${name}을 입력하세요.` : "",
+        }));
     }
 
     const handleSigninInputOnChange = (e) => {
@@ -35,37 +46,54 @@ function Signin(props) {
     }
 
     const handleSigninButtonOnClick = async () => {
-
-        if (Object.entries(errors).filter(entry => entry[1]) ) {
-           
+        if(Object.entries(errors).filter(entry => !!entry[1]).length > 0) {
             return;
         }
 
         try {
-            
-            const response = api.post("/api/auth/signin", signinInput)
+            const response = await api.post("/api/auth/signin", signinInput);
             console.log(response);
-            
-        } catch (error) {
-            console.log(error);
-            
+
+            const accessToken = response.data.data;
+            setAccessToken(accessToken);
+            queryClient.invalidateQueries({queryKey: ["userQuery"]});
+            setSigninError(false);
+            navigate("/");
+            // window.location.href = "/";
+        } catch(error) {
+            setSigninError(true);
         }
-        
     }
 
     return (
         <Box mt={10}>
-            <Container>
+            <Container maxWidth={"xs"}>
                 <Card variant='outlined'>
                     <CardContent>
                         <Typography variant='h4' textAlign={'center'}>로그인</Typography>
                         <Box display={"flex"} flexDirection={'column'} gap={2}>
-                            <TextField type='text' label="username" name='username' onChange={handleSigninInputOnChange} value={signinInput.username} 
-                            onBlur={handleInputOnBlur} />
-                            <TextField type='password' label="password" name='password' onChange={handleSigninInputOnChange} value={signinInput.password} 
-                            onBlur={handleInputOnBlur} />
+                            <TextField type='text' label="username" name='username' 
+                                onChange={handleSigninInputOnChange} value={signinInput.username}
+                                onBlur={handleInputOnBlur}
+                                error={!!errors.username}
+                                helperText={errors.username} />
+                            <TextField type='password' label="password" name='password' 
+                                onChange={handleSigninInputOnChange} value={signinInput.password}
+                                onBlur={handleInputOnBlur}
+                                error={!!errors.password}
+                                helperText={errors.password} />
+                            {
+                                isSigninError && 
+                                <Typography variant='body2' textAlign={'center'} color='red'>
+                                    사용자 정보를 다시 확인하세요.
+                                </Typography>
+                            }
+                            
                             <Button variant='contained' onClick={handleSigninButtonOnClick}>로그인</Button>
                         </Box>
+                        <Typography variant='h6' textAlign={'center'}>
+                            계정이 없으신가요? <Link to={"/signup"}>회원가입</Link>
+                        </Typography>
                     </CardContent>
                 </Card>
             </Container>
@@ -73,4 +101,4 @@ function Signin(props) {
     );
 }
 
-export default Signin;
+export default SigninPage;
--- src/routes/AuthRoute/AuthRoute.jsx
+++ src/routes/AuthRoute/AuthRoute.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Signin from '../../pages/Signin/Signin';
-import Signup from '../../pages/Signup/Signup';
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import SigninPage from '../../pages/SigninPage/SigninPage';
+import SignupPage from '../../pages/SignupPage/SignupPage';
+import { useQueryClient } from '@tanstack/react-query';
 
 function AuthRoute(props) {
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+    console.log(queryClient.getQueryState(["userQuery"]));
+    console.log(queryClient.getQueryData(["userQuery"]));
+    const isLogin = !!queryClient.getQueryData(["userQuery"]);
 
-    
+    useEffect(() => {
+        if(isLogin) {
+            navigate("/");
+        }
+    }, []);
+
     return (
-        <Routes>
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />
-        </Routes>
+        <>
+            {
+                !isLogin &&
+                <Routes>
+                    <Route path="/signin" element={<SigninPage />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                </Routes>
+            }
+        </>
     );
 }
 
